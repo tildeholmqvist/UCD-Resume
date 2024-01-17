8671dece74bb5f29f0aa6399e03ff072982ace72
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-01-17 14:38:33 +0000

    added the fetchgithubinformation function

---
+++ assets/js/github-information.js
@@ -0,0 +1,9 @@
+function fetchGithubInformation(event) {
+    var username = $("#gh-username").value();
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a Github username</h2>`);
+        return; 
+    }
+
+    $("#gh-user-data").html(`<div id="loader"><img src="assets/css/loader.gif"> alt="loading..."/></div>`);
+}
